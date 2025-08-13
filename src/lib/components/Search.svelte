d36cba91a3b7791f001ca32247d86bf137426377
mutindarisper <mutindarisper@gmail.com>
2025-08-13 17:13:08 +0300

    search an AOI and add it to the map

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+   
+
+    let { searchTerm = $bindable(), searchResults = $bindable() }: {
+        searchTerm: string;
+        searchResults: any | null;
+    } = $props();
+
+</script>
+
+<div>
+    <input
+        class="bg-slate-200 w-full p-2 border rounded-lg"
+        bind:value={searchTerm}
+        type="text"
+        placeholder="Search for places"
+    />
+</div>
